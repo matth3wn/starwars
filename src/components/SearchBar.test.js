80268d79842cc68669e38009606d3db11cbf2500
Matt Nguyen <matth3wn@gmail.com>
2019-02-15 11:45:19 -0800

    added more test

--- src/components/SearchBar.test.js
+++ src/components/SearchBar.test.js
@@ -8,13 +8,26 @@ describe(`SearchBar Component`, () => {
 
   it('renders without errors', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<SearchBar search={(e, c) => this.handleSearch(e, c)} />, div);
+    ReactDOM.render(<SearchBar  />, div);
     ReactDOM.unmountComponentAtNode(div);
   })
 
   it('renders searchbar', () => {
     const wrapper = shallow(<SearchBar  />)
-    wrapper.find('button').at(0).simulate('submit')
+    wrapper.find('button').at(0).simulate('submit',)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('expect submit to be called', () => {
+    const preventDefault = jest.fn()
+    const wrapper = shallow(<SearchBar />)
+    wrapper.find('form').simulate('submit',{preventDefault})
+    expect(preventDefault).toBeCalled()
+  })
+
+  it('expect change to be called', () => {
+    const wrapper = shallow(<SearchBar />);
+    wrapper.find('select').simulate('change',{target:{ value: 'tacos'}})
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
